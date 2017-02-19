16033df2dcd06fdfee2b97bb199081f3e1162617
bison01 <amol.bidrolkar@gmail.com>
2017-02-19 14:27:40 +0300

    first commit

---
+++ timepass/src/timepass/GoogleResults.java
@@ -0,0 +1,112 @@
+
+/*
+    Author : Amol Bidrolkar
+	Date Created : 24/11/2016
+	Problem Definition : To search the query from user inputs for specified number of links from search engine and store the result in a file
+
+*/
+package timepass;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class GoogleResults {
+
+	public static final String GOOGLE_SEARCH_URL = "https://www.google.com/search";
+	
+	public static void main(String[] args) throws IOException {
+		//Taking search term input from console
+		
+		PrintWriter pw = null;
+		String outputFolder = ".";
+		File output = null;
+		String fileName="A";
+		    
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Please enter the search term.");
+		String searchTerm = scanner.nextLine();
+		
+		System.out.println("Please enter the number of results. Example: 5, 10, 15,  20");
+		
+		int num=0;
+		num = scanner.nextInt();
+		//scanner.close();
+	
+		System.out.println("Number of links requested : "+num);
+		
+		String searchURL = GOOGLE_SEARCH_URL + "?q="+searchTerm+"&num="+num;      
+		//without proper User-Agent, we will get 403 error
+		Document doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+		
+		//below will print HTML data, save it to a file and open in browser to compare
+		//System.out.println(doc.html());
+		
+		//If google search results HTML change the <h3 class="r" to <h3 class="r1"
+		//we need to change below accordingly
+		Elements results = doc.select("h3.r > a");
+
+		int rs=results.size()-1;
+		System.out.println(" Results size : "+rs);
+		
+		try {
+	        File dir = new File(outputFolder + '/' + "HTMLSources");
+	        if (!dir.exists()) {
+	            boolean success = dir.mkdirs();
+	            if (success == false) {
+	                try {
+	                    throw new Exception(dir + " could not be created");
+	                } catch (Exception e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+
+	        output = new File(dir + "/" + fileName);
+	        if (!output.exists()) {
+	            try {
+	                output.createNewFile();
+	            } catch (IOException ioe) {
+	                ioe.printStackTrace();
+	            }
+	        }
+	        pw = new PrintWriter(new FileWriter(output, true));
+	     
+	        for (int i = 0; i < results.size()-1; i++)
+	        {
+				String linkHref = results.get(i).attr("href");
+				String linkText = results.get(i).text();
+				//System.out.println("\n Link "+i+" Search  Text Result :" + linkText + ", URL is :" + linkHref);
+				//System.out.println("\n Link "+i+" Search  Text Result :" + linkText + ", URL is :" + linkHref.substring(6, linkHref.indexOf("&")));
+				pw.print("\n Link "+i+" Search  Text Result :" + linkText + ", URL is :" + linkHref.substring(6, linkHref.indexOf("&")));
+	            pw.print("\n");
+			}
+	      } catch (IOException ioe) {
+	        ioe.printStackTrace();
+	    } finally {
+	        pw.close();
+	    }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
+
+
+
+
